ebba03bce5157629f3f030fe7130241895d74649
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2025-04-14 15:18:00 +0300

    Чат.

--- Server/src/ru/smak/chat/ConnectedClient.kt
+++ Server/src/ru/smak/chat/ConnectedClient.kt
@@ -1,23 +1,32 @@
 package ru.smak.chat
 
-import java.io.PrintWriter
 import java.net.Socket
-import java.util.*
-import kotlin.concurrent.thread
 
 class ConnectedClient(val socket: Socket) {
 
     private val communicator = Communicator(socket)
+    private var userName: String? = null
 
     init{
+        connectedClients.add(this)
         communicator.start { message -> parse(message) }
     }
 
     private fun parse(message: String){
-        communicator.sendMessage(message)
+        sendToAll(message, echo = false)
     }
 
     fun stop(){
         communicator.stop()
+    }
+
+    private fun sendToAll(message: String, echo: Boolean = true){
+        connectedClients.forEach {
+            if (echo || it != this) it.communicator.sendMessage(message)
+        }
+    }
+
+    companion object {
+        private val connectedClients = mutableListOf<ConnectedClient>()
     }
 }
